68557066e2e75127391e239cc6bf87299fe0af63
Ribhararnus Pracutian <pracutian@ribhararn.us>
2013-05-18 12:14:40 +0700

    some changes, from https://github.com/oknoorap/tpljs/issues/1

--- tpl.js
+++ tpl.js
@@ -2,7 +2,9 @@ var tpljs = function() {
 	'use strict';
 
 	var
-		_tpljs
+		_tpljs,
+		rcontent = /\/\*([\s\S]*?)\*\//g,
+		rtemplate = new RegExp('\\$([a-zA-Z0-9_]+)', 'g')
 	;
 
 	/* tpljs alias */
@@ -13,43 +15,40 @@ var tpljs = function() {
 	
 	/* add tpl js using function, templating using javascript comments */
 	_tpljs.prototype.add = function(name, fn){
-		_tpljs.prototype.tpl[name] = fn;
+		var
+			rcontentMatch = fn.toString().match(rcontent),
+			content = ''
+		;
+
+		/* Extract Content */
+		if (rcontentMatch != null) {
+			content = rcontentMatch[0];
+			content = content.replace(rcontent, function(m, $1) {
+				return $1.replace("\n", '').trim();
+			});
+		}
+
+		_tpljs.prototype.tpl[name] = content;
 	};
 
 	/* get template and give variable */
 	_tpljs.prototype.view = function(name, obj) {
-		var template = _tpljs.prototype.tpl[name].toString();
+		var template = _tpljs.prototype.tpl[name];
 
 		if( typeof template !== 'undefined' ) {
-			var
-				rcontent = /\/\*([\s\S]*?)\*\//g,
-				rcontentMatch = template.match(rcontent),
-				content
-			;
+			if(typeof obj !== 'undefined') {
+				var
+					i
+				;
 
-			if (rcontentMatch != null) {
-				content = rcontentMatch[0];
-				content = content.replace(rcontent, function(m, $1) {
-					return $1.replace("\n", '').trim();
+				/* Replace all variables with objects */
+				template = template.replace(rtemplate, function(m, $1){
+					if(obj.hasOwnProperty($1)){
+						return obj[$1];
+					};
 				});
-
-				if(typeof obj !== 'undefined') {
-					var
-						i
-					;
-					for(i in obj) {
-						if (obj.hasOwnProperty(i)) {
-							content = content.replace(new RegExp('\\$' + i, 'g'), obj[i]);
-						}
-					}
-
-					return content;
-				}
-
-				return content;
 			}
-
-			return;
+			return template;
 		}
 
 		return;
